c890d9cb7c651ec9dcdafd6bd78318f4b7a6fd0e
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2015-08-22 19:40:26 +0200

    fix not using a module.

--- navigator.ts
+++ navigator.ts
@@ -8,41 +8,43 @@
         handles general back and forward navigation
     <summary>
 */
-class PageNavigator {
-    private history = [];       //Page history
-    private position: number;   //Position in the history
-
-    /*
-        public GoBack(string)
-        <summary>
-            Navigates backward.
-        </summary>
-        return string "page name after navigation"
-    */
-    public GoBack() {
-        return 'page name';
-    }
-
-    /*
-        public GoForward(string)
-        <summary>
-            Navigates forward.
-        </summary>
-        return string "page name after navigation"
-    */
-    public GoForward() {
-        return 'page name';
-    }
-
-    /*
-        public AddRecord(string)
-        pageName string "The page"
-        <summary>
-            Add page to the history.
-        </summary>
-        return boolean "True if successful, else false"
-    */
-    public AddRecord(pageName:string) {
-        return true;
+module NavigatorJs {
+    export class Navigator {
+        private history = [];       //Page history
+        private position: number;   //Position in the history
+    
+        /*
+            public GoBack(string)
+            <summary>
+                Navigates backward.
+            </summary>
+            return string "page name after navigation"
+        */
+        public GoBack() {
+            return 'page name';
+        }
+    
+        /*
+            public GoForward(string)
+            <summary>
+                Navigates forward.
+            </summary>
+            return string "page name after navigation"
+        */
+        public GoForward() {
+            return 'page name';
+        }
+    
+        /*
+            public AddRecord(string)
+            pageName string "The page"
+            <summary>
+                Add page to the history.
+            </summary>
+            return boolean "True if successful, else false"
+        */
+        public AddRecord(pageName: string) {
+            return true;
+        }
     }
 }
